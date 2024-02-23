a344bae815480c7f023251cf856a7159b0e3598a
Juanes2192 <juan.perdomo01@unicatolica.edu.co>
2024-02-23 17:01:37 -0500

    Se crea login y admin navbar

--- Front/dinnerSys-Front/src/pages/login/Login.jsx
+++ Front/dinnerSys-Front/src/pages/login/Login.jsx
@@ -1,128 +1,110 @@
-// import { json } from 'react-router-dom';
-// import { useAuth } from '../../auth/AuthProvider';
-// import './Login.css';
-
-// import React, { useState } from 'react';
-
-// export const Login = () => {
-//   const {setIsAuthenticated} = useAuth();
-
-//   const [formulario, setFormulario] = useState({
-//     nombre: '',
-//     email: '',
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormulario(prevState => ({
-//       ...prevState,
-//       [name]: value
-//     }));
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log('Formulario enviado:', formulario);
-//     localStorage.setItem("User",JSON.stringify({id:2}))
-//     setIsAuthenticated(true);
-//   };
-
-//   return (
-//     <div>
-//       <h2>Formulario de Login en React</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label htmlFor="nombre">Nombre:</label>
-//         <input
-//           type="text"
-//           id="nombre"
-//           name="nombre"
-//           value={formulario.nombre}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <label htmlFor="email">Email:</label>
-//         <input
-//           type="email"
-//           id="email"
-//           name="email"
-//           value={formulario.email}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <button type="submit">Enviar</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-
-
-// Login.js
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { useAuth } from '../../auth/AuthProvider';
 import './Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';;
-
+import { UserCircleIcon } from '../../iconos/UserCircleIcon';
+import UserIcon from '../../iconos/UserIcon';
+import LockIcon from '../../iconos/LockIcon';
 
 export function Login() {
+  const { setIsAuthenticated } = useAuth();
+
+  const [formulario, setFormulario] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errores, setErrores] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormulario((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrores((prevState) => ({
+      ...prevState,
+      [name]: '',
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación del correo electrónico
+    if (!formulario.email.includes('@')) {
+      setErrores((prevState) => ({
+        ...prevState,
+        email: 'Correo no es válido',
+      }));
+      return;
+    }
+
+    // Validación de la contraseña
+    if (formulario.password.trim() === '') {
+      setErrores((prevState) => ({
+        ...prevState,
+        password: 'Debe completar este campo',
+      }));
+      return;
+    }
+
+    console.log('Formulario enviado:', formulario);
+    localStorage.setItem("User", JSON.stringify({ id: 3 }));
+    setIsAuthenticated(true);
+  };
+
   return (
-    <div>
+    <div className='user-form'>
+      <UserCircleIcon />
 
-<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="84" height="84" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7f5345" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-  <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-  <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-</svg>
+      <div className="container-gray">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo electronico</Form.Label>
+            <InputGroup>
+              <InputGroup.Text>
+                <UserIcon />
+              </InputGroup.Text>
+              <FormControl
+                type="text"
+                placeholder="Correo electronico"
+                name="email"
+                value={formulario.email}
+                onChange={handleChange}
+              />
+            </InputGroup>
+            <div className="error-message">{errores.email}</div>
+          </Form.Group>
 
-    <div className="container-gray">
-        
-      
-      <Form>
-      
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <InputGroup>
-            <InputGroup.Text>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7f5345" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-              </svg>
-            </InputGroup.Text>
-            <FormControl type="email" placeholder="Enter email" />
-          </InputGroup>
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <InputGroup>
+              <InputGroup.Text>
+                <LockIcon />
+              </InputGroup.Text>
+              <FormControl
+                type="password"
+                placeholder="Contraseña"
+                name="password"
+                value={formulario.password}
+                onChange={handleChange}
+              />
+            </InputGroup>
+            <div className="error-message">{errores.password}</div>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Contraseña</Form.Label>
-          <InputGroup>
-            <InputGroup.Text>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7f5345" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
-                <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
-                <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
-              </svg>
-            </InputGroup.Text>
-            <FormControl type="password" placeholder="Contraseña" />
-          </InputGroup>
-        </Form.Group>
-
-       
-
-        <Button variant="primary" type="submit">
-          Enviar
-        </Button>
-      </Form>
-    </div>
+          <Button variant="primary" type="submit" className="btn-primary">
+            Iniciar Sesion
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
 
 export default Login;
-
