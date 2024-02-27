91172c7c2106505a12a04c8554ee0061129e1a21
Juanes2192 <juan.perdomo01@unicatolica.edu.co>
2024-02-27 01:09:16 -0500

    Se agrega navbar

--- Front/dinnerSys-Front/src/components/NavbarAdmin.jsx
+++ Front/dinnerSys-Front/src/components/NavbarAdmin.jsx
@@ -7,29 +7,30 @@ export function NavbarAdmin() {
   return (
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">DinnerSys</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              {/* Opciones de Usuarios */}
+              <NavDropdown title="Usuarios" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Crear Usuario</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2"> Listado de Usuarios</NavDropdown.Item>
+              </NavDropdown>
+              {/* Opciones de Productos */}
+              <NavDropdown title="Productos" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Crear Producto</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">Listado de Productos</NavDropdown.Item>
+              </NavDropdown>
+              {/* Opciones de Ventas */}
+              <NavDropdown title="Ventas" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Listado de Ventas</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+              <Nav.Link href="#deets">Perfil</Nav.Link>
+              <Nav.Link href="#deets">Cerrar Sesion</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
