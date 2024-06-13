b03d94d68d12f1126fe071c9c56a0e1f7004ac4c
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-06-13 14:41:30 -0500

    Terminamos la tesis

--- Front/dinnerSys-Front/src/pages/mesero/OpcionesMesero/PedidosMesero/PedidosMesero.jsx
+++ Front/dinnerSys-Front/src/pages/mesero/OpcionesMesero/PedidosMesero/PedidosMesero.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Table, InputGroup, FormControl, Button, Container, Alert } from 'react-bootstrap';
 import './PedidosMesero.css';
 import { getProducts } from '../../../../API/Productos';
-import { createOrder, getOrderXTableId } from '../../../../API/Pedidos';
+import { createOrder, getOrderXTableId, updateOrder } from '../../../../API/Pedidos';
 import { freeTable } from '../../../../API/Mesas';
-import { useAuth } from '../../../../auth/AuthProvider'; // Ajusta la ruta según la estructura de tu proyecto
+import { useAuth } from '../../../../auth/AuthProvider';
 
 function PedidosMesero({ mesa, pedido }) {
-  const { UserId } = useAuth(); // Obteniendo UserId del contexto de autenticación
+  const { UserId } = useAuth();
 
   const [filtros, setFiltros] = useState({
     nombre: '',
@@ -18,6 +18,7 @@ function PedidosMesero({ mesa, pedido }) {
   const [productosDisponibles, setProductosDisponibles] = useState([]);
   const [productosEnPedido, setProductosEnPedido] = useState([]);
   const [errorMensaje, setErrorMensaje] = useState('');
+  const [errorActualizarPedido, setErrorActualizarPedido] = useState(false); // Nuevo estado para controlar error de actualización
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,7 +41,7 @@ function PedidosMesero({ mesa, pedido }) {
     if (pedido && pedido.length > 0 && pedido[0].productos) {
       const productosExistentes = pedido[0].productos.map(p => ({
         ...p,
-        id: Date.now() + Math.random(), // Generar un ID único para cada producto
+        id: Date.now() + Math.random(),
         cantidad: p.cantidad || 1
       }));
       setProductosEnPedido(productosExistentes);
@@ -64,10 +65,30 @@ function PedidosMesero({ mesa, pedido }) {
     }
   };
 
-  const eliminarProducto = (id) => {
+  const eliminarProducto = async (id) => {
     const nuevosProductosEnPedido = productosEnPedido.filter(producto => producto.id !== id);
     setProductosEnPedido(nuevosProductosEnPedido);
     localStorage.setItem(`productosEnPedidoMesa${mesa}`, JSON.stringify(nuevosProductosEnPedido));
+
+    // Actualizar pedido en el servidor solo si ya se ha creado
+    if (pedido && pedido.length > 0) {
+      const pedidoActualizado = {
+        MeseroId: UserId,
+        MesaId: mesa,
+        lstProductos: nuevosProductosEnPedido.map(producto => ({ ProductoId: producto.ProductoId, Cantidad: producto.cantidad }))
+      };
+
+      try {
+        const isUpdate = await updateOrder(pedido[0].PedidoId, pedidoActualizado);
+        if (!isUpdate) {
+          setErrorActualizarPedido(true); // Mostrar error específico para la actualización
+        } else {
+          setErrorActualizarPedido(false); // Restablecer el estado de error de actualización
+        }
+      } catch (error) {
+        setErrorActualizarPedido(true); // Mostrar error específico para la actualización
+      }
+    }
   };
 
   const actualizarCantidadProducto = (id, cantidad) => {
@@ -81,7 +102,7 @@ function PedidosMesero({ mesa, pedido }) {
   const crearPedido = async () => {
     try {
       const newOrder = {
-        MeseroId: UserId, // Usando UserId del contexto de autenticación
+        MeseroId: UserId,
         MesaId: mesa,
         lstProductos: productosEnPedido.map(producto => ({ ProductoId: producto.ProductoId, Cantidad: producto.cantidad }))
       };
@@ -89,6 +110,7 @@ function PedidosMesero({ mesa, pedido }) {
       if (isCreated) {
         console.log("Pedido creado correctamente");
         setErrorMensaje('');
+        window.location.reload();
       } else {
         setErrorMensaje('Error al crear el pedido. Por favor, intenta de nuevo.');
       }
@@ -105,10 +127,6 @@ function PedidosMesero({ mesa, pedido }) {
     eliminarProducto(id);
   };
 
-  const handleGenerarPedido = () => {
-    // Lógica para generar el recibo del pedido
-  };
-
   const handleFiltrar = (e, campo) => {
     setFiltros({ ...filtros, [campo]: e.target.value });
   };
@@ -123,146 +141,143 @@ function PedidosMesero({ mesa, pedido }) {
     await freeTable(mesa, pedido[0].PedidoId);
     localStorage.removeItem(`productosEnPedidoMesa${mesa}`);
     setProductosEnPedido([]);
+    window.location.reload();
   };
 
   const calcularTotal = () => {
     return productosEnPedido.reduce((total, producto) => total + (producto.Precio * producto.cantidad), 0);
   };
+
   return (
     <div className="pedidos-mesero-container">
-    <div className="listado-productos-header">
-      <h1>Crear Pedido</h1>
-    </div>
-    <div className="listado-productos-header">
-      <h2>Pedido Mesa: {mesa}</h2>
-    </div>
-    <Container>
-      <InputGroup className="listado-productos-input">
-        <FormControl
-          placeholder="Filtrar por nombre..."
-          value={filtros.nombre}
-          onChange={(e) => handleFiltrar(e, 'nombre')}
-        />
-        <FormControl
-          placeholder="Filtrar por categoría..."
-          value={filtros.categoria}
-          onChange={(e) => handleFiltrar(e, 'categoria')}
-        />
-        <FormControl
-          placeholder="Filtrar por precio..."
-          value={filtros.precio}
-          onChange={(e) => handleFiltrar(e, 'precio')}
-        />
-      </InputGroup>
+      <div className="listado-productos-header">
+        <h1>Crear Pedido</h1>
+      </div>
+      <div className="listado-productos-header">
+        <h2>Pedido Mesa: {mesa}</h2>
+      </div>
+      <Container>
+        <InputGroup className="listado-productos-input">
+          <FormControl
+            placeholder="Filtrar por nombre..."
+            value={filtros.nombre}
+            onChange={(e) => handleFiltrar(e, 'nombre')}
+          />
+          <FormControl
+            placeholder="Filtrar por categoría..."
+            value={filtros.categoria}
+            onChange={(e) => handleFiltrar(e, 'categoria')}
+          />
+          <FormControl
+            placeholder="Filtrar por precio..."
+            value={filtros.precio}
+            onChange={(e) => handleFiltrar(e, 'precio')}
+          />
+        </InputGroup>
 
-      <div className="listado-productos-scroll-container">
-        <Table striped bordered hover className="listado-productos-table">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Descripción</th>
-              <th>Categoría</th>
-              <th>Precio</th>
-              <th>Pedido</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productosFiltrados.map(producto => (
-              <tr key={producto.ProductoId}>
-                <td>{producto.Nombre}</td>
-                <td>{producto.Descripcion}</td>
-                <td>{producto.Categoria}</td>
-                <td>{producto.Precio}</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    className="listado-productos-button listado-productos-button-update"
-                    onClick={() => agregarProducto(producto)}
-                  >
-                    Añadir
-                  </Button>
-                </td>
+        <div className="listado-productos-scroll-container">
+          <Table striped bordered hover className="listado-productos-table">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Categoría</th>
+                <th>Precio</th>
+                <th>Pedido</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+            </thead>
+            <tbody>
+              {productosFiltrados.map(producto => (
+                <tr key={producto.ProductoId}>
+                  <td>{producto.Nombre}</td>
+                  <td>{producto.Descripcion}</td>
+                  <td>{producto.Categoria}</td>
+                  <td>{producto.Precio}</td>
+                  <td>
+                    <Button
+                      variant="primary"
+                      className="listado-productos-button listado-productos-button-update"
+                      onClick={() => agregarProducto(producto)}
+                    >
+                      Añadir
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
 
-      <div className="btn-container-crearpedido">
-        <Button
-          variant="success"
-          className="listado-productos-button listado-productos-button-update btn-crear"
-          onClick={crearPedido}
-        >
-          Crear Pedido
-        </Button>
-      </div>
-    </Container>
-    <Container>
-      <div className="listado-productos-scroll-container">
-        <Table striped bordered hover className="listado-productos-table">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Descripción</th>
-              <th>Categoría</th>
-              <th>Valor</th>
-              <th>Cantidad</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productosEnPedido.map((producto) => (
-              <tr key={producto.id}>
-                <td>{producto.Nombre}</td>
-                <td>{producto.Descripcion}</td>
-                <td>{producto.Categoria}</td>
-                <td>{producto.Precio}</td>
-                <td>
-                  <FormControl
-                    type="number"
-                    value={producto.cantidad}
-                    min="1"
-                    onChange={(e) => actualizarCantidadProducto(producto.id, parseInt(e.target.value))}
-                  />
-                </td>
-                <td>
-                  <Button
-                    variant="danger"
-                    className="listado-productos-button listado-productos-button-update btn-quitar"
-                    onClick={() => handleQuitar(producto.id)}
-                  >
-                    Quitar
-                  </Button>
-                </td>
+        <div className="btn-container-crearpedido">
+          <Button
+            variant="success"
+            className="listado-productos-button listado-productos-button-update btn-crear"
+            onClick={crearPedido}
+          >
+            Crear Pedido
+          </Button>
+        </div>
+      </Container>
+      <Container>
+        <div className="listado-productos-scroll-container">
+          <Table striped bordered hover className="listado-productos-table">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Categoría</th>
+                <th>Valor</th>
+                <th>Cantidad</th>
+                <th>Acción</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
-
-      <div className="total-container">
-        <h3>Total: ${calcularTotal().toLocaleString('es-ES', {
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 2  // Máximo 2 decimales
-        })}</h3>
-      </div>
-      <div className="btn-container-liberar">
-        <Button
-          variant="danger"
-          className="listado-productos-button listado-productos-button-update btn-liberar"
-          onClick={liberarMesa}
-        >
-          Liberar Mesa
-        </Button>
-      </div>
-      {errorMensaje && (
-        <Alert variant="danger" className="mt-3">
-          {errorMensaje}
-        </Alert>
-      )}
-    </Container>
-  </div>
+            </thead>
+            <tbody>
+              {productosEnPedido.map((producto) => (
+                <tr key={producto.id}>
+                  <td>{producto.Nombre}</td>
+                  <td>{producto.Descripcion}</td>
+                  <td>{producto.Categoria}</td>
+                  <td>{producto.Precio}</td>
+                  <td>
+                    <FormControl
+                      type="number"
+                      value={producto.cantidad}
+                      onChange={(e) => actualizarCantidadProducto(producto.id, parseInt(e.target.value))}
+                      min="1"
+                    />
+                  </td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      className="btn-quitar"
+                      onClick={() => handleQuitar(producto.id)}
+                    >
+                      Quitar
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <div className="text-center mt-3">
+          <Button
+            variant="primary"
+            className="btn-liberar"
+            onClick={liberarMesa}
+          >
+            Liberar Mesa
+          </Button>
+        </div>
+        <div className="total-container">
+          <h3>Total: ${calcularTotal().toLocaleString('es-ES', {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2  // Máximo 2 decimales
+          })}</h3>
+        </div>
+        {errorMensaje && <Alert variant="danger" className="mt-3 text-center">{errorMensaje}</Alert>}
+      </Container>
+    </div>
   );
 }
 
