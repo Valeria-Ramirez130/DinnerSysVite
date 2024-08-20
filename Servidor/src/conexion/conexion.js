92a2410b23551efd7624f9d6b471447d117dd725
Valeria-Ramirez130 <116677886+Valeria-Ramirez130@users.noreply.github.com>
2024-08-20 17:39:47 -0500

    se agrega las rutas y categorias

--- Servidor/src/conexion/conexion.js
+++
@@ -1,9 +0,0 @@
-import mariadb from 'mariadb';
-
-export const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DinnerSys',
-    port: 3306,
-});
